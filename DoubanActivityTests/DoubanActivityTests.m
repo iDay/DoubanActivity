1c3f73eb05cccfd5908b830790110aa416647934
iDay <johnny.iday@gmail.com>
2013-12-06 14:12:19 +0800

    Initial Commit

---
+++ DoubanActivityTests/DoubanActivityTests.m
@@ -0,0 +1,34 @@
+//
+//  DoubanActivityTests.m
+//  DoubanActivityTests
+//
+//  Created by Johnny iDay on 13-12-6.
+//  Copyright (c) 2013年 Johnny iDay. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DoubanActivityTests : XCTestCase
+
+@end
+
+@implementation DoubanActivityTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
